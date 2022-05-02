76f31fb779ecbd9aa1e8419e1d36e6a5074facb8
sunitanep <bishaller@gmail.com>
2022-05-03 01:11:29 +0545

    wip:footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,7 +19,50 @@ const Footer = () => {
             </svg>
           </a>
           <div className="footer__social">
-
+            <a
+              className="socialIcons socialIcons--twitter"
+              title="Connect with Bishal on Twitter"
+              href="https://twitter.com/twitter"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <span className="screen-reader-text">
+                Connect with Bishal on Twitter
+              </span>
+            </a>
+            <a
+              className="socialIcons socialIcons--linkedin"
+              title="Connect with Bishal on Linkedin"
+              href="https://linkedin.com/in/bishaller"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <span className="screen-reader-text">
+                Connect with Bishal on Linkedin
+              </span>
+            </a>
+            <a
+              className="socialIcons socialIcons--instagram"
+              title="Connect with Bishal on Instagram"
+              href="https://instagram.com/bishaller"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <span className="screen-reader-text">
+                Connect with Bishal on D
+              </span>
+            </a>
+            <a
+              className="socialIcons socialIcons--dribbble"
+              title="Connect with Bishal on Dribbble"
+              href="https://dribbble.com/bishaller"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <span className="screen-reader-text">
+                Connect with Bishal on Dribbble
+              </span>
+            </a>
           </div>
         </div>
       </div>
