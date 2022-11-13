dca0b58ba87e86ced1e2d14071b11ad32bc80c7f
Bishal Mishra <bishaller@gmail.com>
2022-11-13 06:41:54 -0500

    wip: project cards

--- src/components/home/projectPreview.js
+++ src/components/home/projectPreview.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import dwtBG from "../../images/dwt-bg.jpg"
+import wpBG from "../../images/wordcamp.jpg"
+import tpnBG from "../../images/tpn.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 import Marquee from "react-fast-marquee";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
@@ -75,12 +77,94 @@ const ProjectPreview = (linkData) => {
               </AniLink>
             </article>
             <div className="projectPreview__wrap projectPreview__wrap--two">
-              <article className="projectCard">
+              <article className="projectCard projectCard--traqit">
+                <AniLink
+                  to="https://traqit.com/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+                <span className="projectCard__soon">Coming Soon</span>
+                <h2>Event management simplified.</h2>
+                <StaticImage
+                  src={"../../images/traqit.png"}
+                  alt=""
+                  width={5000}
+                  className="projectCard__image"
+                />
               </article>
-              <article className="projectCard">
+              <article className="projectCard projectCard--wp" style={{ backgroundImage: `url(${wpBG})` }}>
+                <AniLink
+                  to="https://kathmandu.wordcamp.org/2018/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
               </article>
             </div>
+
           </div>
+          <div className="projectPreview__wrap projectPreview__wrap--three">
+              <article className="projectCard projectCard--sampression">
+                <AniLink
+                  to="https://sampression.com/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+                <span className="projectCard__soon">Coming Soon</span>
+                <h2>Simplified<br/>no-code websites</h2>
+                <StaticImage
+                  src={"../../images/sampression.png"}
+                  alt=""
+                  width={5000}
+                  className="projectCard__image"
+                />
+              </article>
+              <div className="projectPreview__wrap projectPreview__wrap--vertical">
+              <article className="projectCard projectCard--wp" style={{ backgroundImage: `url(${wpBG})` }}>
+                <AniLink
+                  to="https://idealaya.co/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+              </article>
+              <article className="projectCard projectCard--wp" style={{ backgroundImage: `url(${wpBG})` }}>
+                <AniLink
+                  to="https://kathmandu.wordcamp.org/2018/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+              </article>
+              <article className="projectCard projectCard--wp" style={{ backgroundImage: `url(${tpnBG})` }}>
+                <AniLink
+                  to="https://trekkersparadisenepal.com/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+              </article>
+              </div>
+              <article className="projectCard projectCard--sites">
+                <span className="projectCard__soon">Coming Soon</span>
+                <h2>How might we build a chain of websites that is so similar yet different to each other and resemble their uniqueness.</h2>
+                <AniLink
+                  to="https://kathmandu.wordcamp.org/2018/"
+                  className="projectCard__link"
+                  target="_blank"
+                >
+                  <span className="screen-reader-text">Visit Website</span>
+                </AniLink>
+              </article>
+            </div>
         </div>
       </div>
     </section>
